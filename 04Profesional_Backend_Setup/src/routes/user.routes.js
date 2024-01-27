d11229ffcf348422370b5e765dcc0b6558cf3fcf
moaz-shamim <moaz.shamim1575@gmail.com>
2024-01-27 21:23:36 +0530

    routes for Login & Logout

--- 04Profesional_Backend_Setup/src/routes/user.routes.js
+++ 04Profesional_Backend_Setup/src/routes/user.routes.js
@@ -1,21 +1,35 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// "/register" route for registerUser functionality of user.controller
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    );
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
+
+// "/login" route for loginUser functionality of user.controller
+
+router.route("/login").post(loginUser);
+
+// "/logout" route for loginUser functionality of user.controller
+
+router.route("/logout").post(verifyJWT, logoutUser);
