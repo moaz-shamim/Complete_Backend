ea5d9b92b3a1f6502988dd93df6d0419e089f7a5
moaz-shamim <moaz.shamim1575@gmail.com>
2024-01-28 13:37:20 +0530

    Adding Route for refreshing access tokens

--- 04Profesional_Backend_Setup/src/routes/user.routes.js
+++ 04Profesional_Backend_Setup/src/routes/user.routes.js
@@ -3,14 +3,18 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
+// Creating an instance of Express Router
 const router = Router();
 
-// "/register" route for registerUser functionality of user.controller
+// Route for user registration ("/register")
+// Handles the registration functionality provided by the user.controller module
 router.route("/register").post(
+  // Middleware for handling file uploads (avatar and coverImage)
   upload.fields([
     {
       name: "avatar",
@@ -21,15 +25,21 @@ router.route("/register").post(
       maxCount: 1,
     },
   ]),
+  // Controller function responsible for user registration
   registerUser
 );
 
 export default router;
 
-// "/login" route for loginUser functionality of user.controller
-
+// Route for user login ("/login")
+// Handles the login functionality provided by the user.controller module
 router.route("/login").post(loginUser);
 
-// "/logout" route for loginUser functionality of user.controller
-
+// Route for user logout ("/logout")
+// Requires JWT verification (middleware) to ensure a valid user session
+// Handles the logout functionality provided by the user.controller module
 router.route("/logout").post(verifyJWT, logoutUser);
+
+// Route for refreshing access tokens ("/refresh-token")
+// Handles the token refreshing functionality provided by the user.controller module
+router.route("/refresh-token").post(refreshAccessToken);
